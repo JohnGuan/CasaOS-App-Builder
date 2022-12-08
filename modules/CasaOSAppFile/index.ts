81036942b68f23139c5d1011134e784b63b9d9ca
JohnGuan <guan.ningchuan@gmail.com>
2022-12-08 18:25:31 +0800

    Update index.ts

--- modules/CasaOSAppFile/index.ts
+++ modules/CasaOSAppFile/index.ts
@@ -135,6 +135,36 @@ export const appCategory = [
   },
 ];
 
+export const appCaps = [
+  "AUDIT_CONTROL",
+  "AUDIT_READ",
+  "BLOCK_SUSPEND",
+  "BPF",
+  "CHECKPOINT_RESTORE",
+  "DAC_READ_SEARCH",
+  "IPC_LOCK",
+  "IPC_OWNER",
+  "LEASE",
+  "LINUX_IMMUTABLE",
+  "MAC_ADMIN",
+  "MAC_OVERRIDE",
+  "NET_ADMIN",
+  "NET_BROADCAST",
+  "PERFMON",
+  "SYS_ADMIN",
+  "SYS_BOOT",
+  "SYS_MODULE",
+  "SYS_NICE",
+  "SYS_PACCT",
+  "SYS_PTRACE",
+  "SYS_RAWIO",
+  "SYS_RESOURCE",
+  "SYS_TIME",
+  "SYS_TTY_CONFIG",
+  "SYSLOG",
+  "WAKE_ALARM"
+]
+
 const [useAppData, SharedAppDataProvider] = createStateContext<AppFile2>({
   version: "2.0",
 } as AppFile2);
